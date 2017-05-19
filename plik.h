b32b43a4e87eccfc5f8b3c579abb05e6b53d68e3
kingata <kintal.student@gmail.com>
2017-05-19 20:53:56 +0200

    Interfejs

--- plik.h
+++ plik.h
@@ -2,3 +2,24 @@
 #include <stdlib>
 
 using namespace std;
+
+
+class Interface : public QWidget
+{
+  Q_OBJECT
+  public:
+        Interface();
+        ~Interface();
+  public slots:
+        void FsaveOn();
+        void FsaveOff();
+  private:
+        Game *game;
+
+        QPushButton *pbNowa_gra;
+        QPushButton *pbZapisz_gre;
+        QPushButton *pbWczytaj_gre;
+        QPushButton *pbWyjscie;
+        QLabel *infoLabel;
+        QLabel *autorLabel;
+};
