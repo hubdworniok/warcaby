2c2bbe7e0aff02b96d520c0b28441c5d3d0c9915
Filip <garbacikfilip@gmail.com>
2017-05-22 23:46:38 +0200

    Dodaję osobny plik z początkiem header klasy GRA.

---
+++ plik2.h
@@ -0,0 +1,28 @@
+#ifndef PLIK2_H_INCLUDED
+#define PLIK2_H_INCLUDED
+
+#include <iostream>
+#include <stdlib>
+
+using namespace std;
+
+class Game : public QWidget
+{
+    Q_OBJECT;
+
+    public:
+        Game();
+        ~Game();
+
+        //tutaj wypisaæ nazwy funkcji potrzebnych do gry, które bêd¹ stworzone w plik.cpp//
+
+    private:
+
+        //nazwy zmiennych//
+};
+
+
+
+
+
+#endif // PLIK2_H_INCLUDED
