2f97fe02bfdd97d04337ed21eada0d12ee001005
Hubert Dworniok <hubdworniok@gmail.com>
2017-05-30 14:24:24 +0200

    Przygotowanie kodu - aktualizacja gra.h

--- kod/gra.h
+++ kod/gra.h
@@ -1,3 +1,8 @@
+/*
+ * Plik nag?ówkowy gra.h
+*/
+
+
 #ifndef GRA_H_INCLUDED
 #define GRA_H_INCLUDED
 
@@ -6,19 +11,71 @@
 
 using namespace std;
 
-class Game : public QWidget
-{
-    Q_OBJECT;
+class Gra : public QWidget {
 
-    public:
-        Game();
-        ~Game();
+        Q_OBJECT;
 
-        //tutaj wypisaæ nazwy funkcji potrzebnych do gry, które bêd¹ stworzone w gra.cpp//
+        public:
+                Gra();
+                ~Gra();
 
-    private:
-        int Punkty_1;
-        int Punkty_2;
+                void zerujPlansza();
+                void zerujZbite();
+                void zerujMRPionek();
+                void zerujMRPole();
+
+                void sprawdzMRPionek(int kolor);
+                void sprawdzMBPionek(int kolor);
+                void sprawdzMRPole(int x, int y, int pionek);
+                void sprawdzMBPole(int x, int y, int pionek);
+
+                void mouseMoveEvent(QMouseEvent *event);
+                void mousePressEvent(QMouseEvent *event);
+                void paintEvent(QPaintEvent*);
+
+        public slots:
+                void nowagra();
+                void zapiszgra();
+                void wczytajgre();
+        signals:
+                void zmianaEtap(QString info);
+                void saveOn();
+                void saveOff();
+        private:
+                int i;
+                int j;
+
+                int xPos;
+                int yPos;
+
+                int plansza[12][12];
+                int zbite[12][12];
+                int MRPionek[8][8];
+                int MRPole[8][8];
+
+                int tmp;
+                int etap;
+                int bicie;
+                int bicieNext;
+};
+
+class Interfejs : public QWidget {
+  Q_OBJECT
+  public:
+        Interfejs();
+        ~Interfejs();
+  public slots:
+        void FsaveOn();
+        void FsaveOff();
+  private:
+        Game *game;
+
+        QPushButton *pbNowa_gra;
+        QPushButton *pbZapisz_gre;
+        QPushButton *pbWczytaj_gre;
+        QPushButton *pbWyjscie;
+        QLabel *infoLabel;
+        QLabel *autorLabel;
 };
 
 
